dcaa7033649302d318142b7ba67c70c60229b63c
Andres DLRG <cristian.rueg@uniagustiniana.edu.co>
2025-11-03 23:36:57 +0000

    correccion de errores

--- proyecto/src/main.js
+++ proyecto/src/main.js
@@ -1,7 +1,9 @@
 import './style.css'
-// src/main.js
+
 import { mostrarRegistro } from './register.js';
 import { mostrarLogin } from './login.js';
 import { mostrarMVP } from './mvp.js';
 import { mostrarUser } from './user.js';
-mostrarUser();
+import { mostrarAdmin } from './admin.js';
+
+mostrarAdmin();
